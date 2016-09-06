a4701471cf19ee5a7ac912fa01822420e7db502e
carloscrovara <carloscrovara@gmail.com>
2016-09-06 16:36:31 -0300

    versión 1.0

--- js/script.js
+++ js/script.js
@@ -2,12 +2,25 @@ var diasDeSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sába
 var diaActual = prompt("Ingresar día");
 
 switch (diaActual) {
+    case diasDeSemana[0]:
+	     console.log("Es día de semana");
+	     break;
+    case diasDeSemana[1]:
+	     console.log("Es día de semana");
+	     break;
+    case diasDeSemana[2]:
+	     console.log("Es día de semana");
+	     break;
+    case diasDeSemana[3]:
+	     console.log("Es día de semana");
+	     break;
+    case diasDeSemana[4]:
+	     console.log("Es día de semana");
+	     break;	     	     	     	     
 	case diasDeSemana[5]:
 	     console.log("Es fin de semana");
 	     break;
 	case diasDeSemana[6]:
 	     console.log("Es fin de semana");
 	     break;
-	default:
-	    console.log("No es sábado o domingo");
 }
